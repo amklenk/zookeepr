97198d1f21f98c88393510656b94f24f4052aac7
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-19 13:43:48 -0400

    added static method to make css and js files static

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static('public'));
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -114,6 +116,10 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
