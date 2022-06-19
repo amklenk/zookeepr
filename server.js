8bc023c050c089599e0143ffaaa99c650e893f33
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-19 10:33:09 -0400

    fixed code for port

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.get("/api/animals", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
 
